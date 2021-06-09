a0d77142a4391013f7a0a9d6299ab4d44d4d2b29
shadman <shadmansutshob@gmail.com>
2021-06-10 00:40:07 +0600

    initial commit

---
+++ app/src/main/java/com/example/todoapp/Model/ToDoModel.java
@@ -0,0 +1,31 @@
+package com.example.todoapp.Model;
+
+public class ToDoModel {
+
+    private String Task;
+    private int id, status ;
+
+    public String getTask() {
+        return Task;
+    }
+
+    public void setTask(String task) {
+        Task = task;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
